3fdef8b73e48849f742a6ab423aefdb257e1bc3c
Alex Siegberg and Mike Lofgren <alex@>
2019-06-04 13:52:29 -0700

    Add buttons and javascript to toggle descriptions.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(".ja").click(function() {
+    $(".ja p").toggle();
+  });
+
+  $(".op").click(function() {
+    $(".op p").toggle();
+  });
+
+  $(".va").click(function() {
+    $(".va p").toggle();
+  });
+});
