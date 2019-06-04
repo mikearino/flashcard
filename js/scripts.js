b5d13b2299848add1c201a1b3062f877aeb84bac
Alex Siegberg and Mike Lofgren <alex@>
2019-06-04 14:53:04 -0700

    Add functionality to buttons, add styling.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,70 @@ $(function() {
   $(".va").click(function() {
     $(".va p").toggle();
   });
+
+  $(".vn").click(function() {
+    $(".vn p").toggle();
+  });
+
+  $(".fu").click(function() {
+    $(".fu p").toggle();
+  });
+
+  $(".me").click(function() {
+    $(".me p").toggle();
+  });
+
+  $(".ar").click(function() {
+    $(".ar p").toggle();
+  });
+
+  $(".pa").click(function() {
+    $(".pa p").toggle();
+  });
+
+  $(".re").click(function() {
+    $(".re p").toggle();
+  });
+
+  $(".ch").click(function() {
+    $(".ch p").toggle();
+  });
+
+  $(".st").click(function() {
+    $(".st p").toggle();
+  });
+
+  $(".bo").click(function() {
+    $(".bo p").toggle();
+  });
+
+  $(".un").click(function() {
+    $(".un p").toggle();
+  });
+
+  $(".na").click(function() {
+    $(".na p").toggle();
+  });
+
+  $(".es").click(function() {
+    $(".es p").toggle();
+  });
+
+  $(".al").click(function() {
+    $(".al p").toggle();
+  });
+
+  $(".co").click(function() {
+    $(".co p").toggle();
+  });
+
+  $(".jq").click(function() {
+    $(".jq p").toggle();
+  });
+
+  $(".at").click(function() {
+    $(".at p").toggle();
+  });
 });
 
 var ourFunction = function (param1, param2) {
